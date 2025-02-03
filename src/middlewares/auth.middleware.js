3d34eb03cc420c469b3de15288bc5ccb11ef1eca
Ayush <ayushempire03@gmail.com>
2025-02-03 19:54:09 +0530

    solved a BUG : used mongodb query insted aggregation

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,8 +3,10 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Employee } from "../models/Employee.models/employee.model.js";
 import { Employer } from "../models/Employer.models/employer.model.js";
-import mongoose from "mongoose";
-
+import { Location } from "../models/location.model.js";
+import { EmployeeSubscription } from "../models/Employee.models/employeesSbscription.model.js";
+import { Experience } from "../models/Employee.models/experience.model.js";
+import Api from "twilio/lib/rest/Api.js";
 export const verifyJWT = asyncHandler(async (req, res, next) => {
     try {
         // checking the accesstokens which is strored in device or in browser
@@ -22,81 +24,116 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
         ); // decoding the tokens //
 
         // finnding users from decoded token id //
-        // const employee = await Employee.findById(decodedToken?._id).select(
-        //     "-password -refreshToken",
-        // );
+        const employee = await Employee.findById(decodedToken?._id).select(
+            "-password -refreshToken",
+        );
+        if (!employee) {
+            throw new ApiError(401, "invalid user please login or register");
+        }
+
+        const location = await Location.find({
+            employee: employee?._id,
+        }).sort({
+            createdAt: -1,
+        });
+        if (!location) {
+            throw new ApiError(404, "location not found");
+        }
+
+        const subscription = await EmployeeSubscription.find({
+            employee: employee?._id,
+        }).sort({
+            createdAt: -1,
+        });
+
+        if (!subscription) {
+            throw new ApiError(404, "subscription not found");
+        }
+
+        const experience = await Experience.find({
+            employee: employee?._id,
+        }).sort({
+            createdAt: -1,
+        });
+        if (!experience) {
+            throw new ApiError("404", "experience not found");
+        }
+
+        employee.subscription = subscription;
+        employee.location = location;
+        employee.workExperience = experience;
 
         // aggregatjion pipeline //
 
-        const employee = await Employee.aggregate([
-            {
-                $match: {
-                    _id: new mongoose.Types.ObjectId(decodedToken?._id),
-                },
-            },
-            {
-                $lookup: {
-                    from: "locations",
-                    localField: "location",
-                    foreignField: "_id",
-                    as: "locations",
-                    pipeline: [
-                        {
-                            $project: {
-                                state: 1,
-                                district: 1,
-                                subDistrict: 1,
-                                pincode: 1,
-                            },
-                        },
-                    ],
-                },
-            },
-            {
-                $lookup: {
-                    from: "experiences",
-                    localField: "workExperience",
-                    foreignField: "_id",
-                    as: "experiences",
-                    pipeline: [
-                        {
-                            $project: {
-                                yearOfExperience: 1,
-                                working: 1,
-                                salary: 1,
-                                jobRole: 1,
-                            },
-                        },
-                    ],
-                },
-            },
-            {
-                $lookup: {
-                    from: "employeeSubscriptions",
-                    localField: "subscription",
-                    foreignField: "_id",
-                    as: "sunscriptions",
-                },
-            },
-            {
-                $project: {
-                    _id: 1,
-                    fName: 1,
-                    lName: 1,
-                    phone: 1,
-                    age: 1,
-                    dateOfBirth: 1,
-                    gender: 1,
-                    email: 1,
-                    avatar: 1,
-                    leades: 1,
-                    education: 1,
-                    locations: 1,
-                    experiences: 1,
-                    subscriptions: 1,
-                },
-            },
-        ]);
+        // const employee = await Employee.aggregate([
+        //     {
+        //         $match: {
+        //             _id: new mongoose.Types.ObjectId(decodedToken?._id),
+        //         },
+        //     },
+        //     {
+        //         $lookup: {
+        //             from: "locations",
+        //             localField: "location",
+        //             foreignField: "_id",
+        //             as: "locations",
+        //             pipeline: [
+        //                 {
+        //                     $project: {
+        //                         state: 1,
+        //                         district: 1,
+        //                         subDistrict: 1,
+        //                         pincode: 1,
+        //                     },
+        //                 },
+        //             ],
+        //         },
+        //     },
+        //     {
+        //         $lookup: {
+        //             from: "experiences",
+        //             localField: "workExperience",
+        //             foreignField: "_id",
+        //             as: "experiences",
+        //             pipeline: [
+        //                 {
+        //                     $project: {
+        //                         yearOfExperience: 1,
+        //                         working: 1,
+        //                         salary: 1,
+        //                         jobRole: 1,
+        //                     },
+        //                 },
+        //             ],
+        //         },
+        //     },
+        //     {
+        //         $lookup: {
+        //             from: "employeeSubscriptions",
+        //             localField: "subscription",
+        //             foreignField: "_id",
+        //             as: "sunscriptions",
+        //         },
+        //     },
+        //     {
+        //         $project: {
+        //             _id: 1,
+        //             fName: 1,
+        //             lName: 1,
+        //             phone: 1,
+        //             age: 1,
+        //             dateOfBirth: 1,
+        //             gender: 1,
+        //             email: 1,
+        //             avatar: 1,
+        //             leades: 1,
+        //             education: 1,
+        //             locations: 1,
+        //             experiences: 1,
+        //             subscriptions: 1,
+        //         },
+        //     },
+        // ]);
         // aggregatjion pipeline //
 
         if (employee.length === 0) {
@@ -105,8 +142,9 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
                 "invaid user,please login again or create account",
             );
         }
-
+        console.log(employee);
         req.employee = employee; // fetched user info is set as coockies
+        console.log(req.employee);
 
         next(); // next midddleware
     } catch (error) {
