d57737dc5b6e16ab2b18e212a0151780a9e9bc42
Ayush <ayushempire03@gmail.com>
2025-02-06 23:37:18 +0530

    created Employee Notification Schema

---
+++ src/models/Employee.models/employeeNotification.model.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+const employeeNotificationSchema = new Schema(
+    {
+        employee: {
+            type: Schema.Types.ObjectId,
+            ref: "Employee",
+        },
+        emploer: {
+            type: Schema.Types.ObjectId,
+            ref: "Employer",
+        },
+
+        title: {
+            type: String,
+            required: true,
+        },
+        message: {
+            type: String,
+            required: true,
+        },
+        read: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamps: true },
+);
+
+export const EmployeeNotification = mongoose.model(
+    "EmployeeNotification",
+    employeeNotificationSchema,
+);
