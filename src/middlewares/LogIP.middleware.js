d1885a22ff5394594ba1a27baf97cda8a490574b
Ayush <ayushempire03@gmail.com>
2025-03-11 14:04:57 +0530

    added some logs information

--- src/middlewares/LogIP.middleware.js
+++ src/middlewares/LogIP.middleware.js
@@ -1,9 +1,26 @@
-import logger from "../utils/logger.js"; // Import logger
+import logger from "../utils/logger.js";
 
 const logClientIP = (req, res, next) => {
     const clientIP = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
-    logger.info(`Request from IP: ${clientIP} - ${req.method} ${req.url}`);
+    // Capture request start time
+    const startTime = Date.now();
+
+    // Capture status after response is sent
+    res.on("finish", () => {
+        const duration = Date.now() - startTime; // Response time
+        const statusCode = res.statusCode;
+
+        if (statusCode >= 200 && statusCode < 400) {
+            logger.info(
+                `[SUCCESS] ${req.method} ${req.url} | IP: ${clientIP} | Status: ${statusCode} | Duration: ${duration}ms`,
+            );
+        } else {
+            logger.error(
+                `[FAILED] ${req.method} ${req.url} | IP: ${clientIP} | Status: ${statusCode} | Duration: ${duration}ms`,
+            );
+        }
+    });
 
     next(); // Continue to the next middleware or route handler
 };
