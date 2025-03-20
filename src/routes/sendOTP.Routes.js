196e29ea68f52aada9b5803c327de34155f13c39
Ayush <ayushempire03@gmail.com>
2025-03-20 15:51:28 +0530

    chnged send otp route

--- src/routes/sendOTP.Routes.js
+++ src/routes/sendOTP.Routes.js
@@ -3,7 +3,9 @@ import { sendOtpEmployee } from "../controllers/sendOTP.controller.js";
 const router = Router();
 
 // send otp route //
-router.route("/otp").post(sendOtpEmployee);
+router.route("/otp").post(() => {
+    "hello";
+});
 // end of send otp route //
 
 export default router;
