f19da449eaaf590210cabcc79875ae2e9409516c
Ayush <ayushempire03@gmail.com>
2025-03-20 15:31:07 +0530

    removed logs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import logClientIP from "./middlewares/LogIP.middleware.js";
+// import logClientIP from "./middlewares/LogIP.middleware.js";
 import { ipBlockerMiddleware } from "./middlewares/blockIP.middleware.js";
 
 const app = express();
@@ -18,7 +18,7 @@ app.use(
 app.use(cookieParser());
 app.use(ipBlockerMiddleware);
 // for loging//
-app.use(logClientIP);
+// app.use(logClientIP);
 // for loging//
 
 // importing routes //
