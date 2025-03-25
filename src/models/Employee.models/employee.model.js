3416bc097f5af8ff7f98f1fb92af73af59ea9a62
Ayush <ayushempire03@gmail.com>
2025-03-25 13:18:25 +0530

    changed maharastra

--- src/models/Employee.models/employee.model.js
+++ src/models/Employee.models/employee.model.js
@@ -73,7 +73,6 @@ const EmployeeSchema = new Schema(
         },
         state: {
             tyep: String,
-            default: "Maharashtra",
         },
         pincode: {
             type: String,
